2c3c0e67d52a4fc56d0550b504bb6d428219c3c1
Rhombus33 <diamond.d.wheeler@gmail.com>
2016-03-26 11:55:20 -0700

    Revert params in angular and server route to the previous version

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -33,7 +33,7 @@ angular.module('GitSpy', [
     templateUrl: '/app/dashboard/dashboard.html',
     controller: 'DashboardController'
   });
-  $routeProvider.when('/:dashboardId/setup', {
+  $routeProvider.when('/:orgName/:repoName/setup', {
     templateUrl: '/app/setup/setup.html',
     controller: 'SetupController'
   });
